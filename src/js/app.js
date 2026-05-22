e58c2c66b864b6c2fe7164695ac028220f61d4a8
mmesab <mmesab99@gmail.com>
2026-05-22 19:29:31 +0200

    Actualizo index.html y app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
+// Seleccionar contenedor
 const container = document.getElementById("question-container");
 
+// Pintar preguntas
 questions.forEach(question => {
     container.innerHTML += `
         <div class="question">
@@ -7,12 +9,57 @@ questions.forEach(question => {
 
             <select id="q-${question.id}">
                 <option value="">Selecciona</option>
-                <option value="1">1 - Nada parecido a mí</option>
-                <option value="2">2 - Poco parecido</option>
-                <option value="3">3 - Depende</option>
-                <option value="4">4 - Bastante parecido</option>
-                <option value="5">5 - Me representa muchísimo</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
             </select>
         </div>
     `;
 });
+
+// Array donde se guardan la resouestas. 
+let answers = [];
+
+// Función para guardar respuestas.
+function saveAnswers() {
+    answers = []; 
+
+    questions.forEach(question => {
+        let value = document.getElementById(`q-${question.id}`).value;
+
+        answers.push({
+            questionId: question.id,
+            answer: Number(value),
+            dimension: question.dimension
+        });
+    });
+    console.log(answers);
+}
+
+
+/*
+    ¿Qué hace esta función? 
+    - Recorre todas las preguntas. 
+    - Busca el <select> correspondiente a cada una. 
+    - Lee el valor elegido. 
+    - Lo convierte a número. 
+    - Lo guarda en el array ANSWERS. 
+
+
+    ¿Cuándo se ejecuta esta función? 
+    Cuando el usuari pulsa el botón "Guardar Respuestas"
+
+    ¿Qué pasa si el usuario no responde una pregunta? 
+    Se valida como NaN. 
+
+    ¿Por qué se guarda también la dimensión? 
+    En el paso 7 se podrá calcular: 
+    - Puntuación total. 
+    - Puntuación por dimensión. 
+    - Perfiles. 
+    - Gráficos. 
+*/
+
+// ---------------------------------------
