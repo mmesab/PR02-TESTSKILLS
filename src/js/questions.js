437b502e029f31f92c47f7c37a366c934dc8777e
mmesab <mmesab99@gmail.com>
2026-05-22 16:47:42 +0200

    Añade questions.js desde maite-rama

---
+++ src/js/questions.js
@@ -0,0 +1,149 @@
+// const questions = [
+//     {
+//       id: 1,    I    DENTIFICACIÓN DE LA PREGUNTA
+//       text : "",     TEXTO DE LA PREGUNTA
+//       dimension: ""  CATEGORÍA DE LA PREGUNTA
+//     } ,      ...... 
+// ]
+
+// FASE 1
+
+const questions = [
+    {
+      id: 1,
+      text : "¿Qué suele impulsarte más?", 
+      dimension: "MotivInter"
+    }, 
+    {
+      id: 2, 
+      text: "Motivación interna", 
+      dimension: "MotivInter"
+    }, 
+    {
+      id: 3, 
+      text: "Cuando algo no sale como esperabas:",
+      dimension: "MotivInter"
+    }, 
+    {
+      id: 4, 
+      text: "¿Qué suele preocuparte más?", 
+      dimension: "MotivInter"
+    }, 
+    {
+      id: 5, 
+      text: "¿Qué describe mejor tu impulso principal?", 
+      dimension: "MotivInter"
+    }, 
+
+    // FASE 2
+
+    {
+      id: 6, 
+      text: "Cuando llegas a un grupo nuevo:", 
+      dimension: "RelacionEntorno"
+    }, 
+    {
+      id: 7, 
+      text: "Relación con el entorno:", 
+      dimension: "RelacionEntorno"
+    }, 
+    {
+      id: 8, 
+      text: "Cuando existe conflicto:", 
+      dimension: "RelacionEntorno"
+    },
+    {
+      id: 9, 
+      text: "¿Qué suele resultarte más difícil?", 
+      dimension: "RelacionEntorno"
+    }, 
+    {
+      id: 10,
+      text: "¿Qué describe mejor tu forma de relacionarte?",
+      dimension: "RelacionEntorno"
+    }, 
+
+    // FASE 3
+
+    {
+      id:11, 
+      text: "Cuando atraviesas estrés importante: ", 
+      dimension: "EstresAdaptacion"
+    }, 
+    {
+      id: 12,
+      text: "Estrés y adaptacón: ", 
+      dimension: "EstresAdaptacion"
+    },
+    {
+      id: 13, 
+      text: "Cuando algo genera incertidumbre: ", 
+      dimension: "EstresAdaptacion"
+    }, 
+    {
+      id: 14, 
+      text: "¿Qué suele darte seguridad?", 
+      dimension: "EstresAdaptacion"
+    }, 
+    {
+      id: 15, 
+      text: "¿Qué describe mejor tu reacción bajo presión?", 
+      dimension: "EstresAdaptacion"
+    }, 
+
+    // FASE 4
+
+    {
+      id: 16, 
+      text: "Cuando alguien cercano tiene problemas:", 
+      dimension: "RelPersonales"
+    }, 
+    {
+      id: 17,
+      text:"Relaciones personales", 
+      dimension: "RelPersonales"
+    }, 
+    {
+      id: 18, 
+      text: "¿Qué valoras más en relaciones humanas?",
+      dimension: "RelPersonales"
+    }, 
+    {
+      id: 19,
+      text: "Cuando alguien rompe tu confianza:", 
+      dimension: "RelPersonales"
+    }, 
+    {
+      id: 20, 
+      text: "Tu forma de vincularte suele ser:", 
+      dimension: "RelPersonales"
+    }, 
+
+    // FASE 5
+
+    {
+      id: 21, 
+      text: "En proyectos profesionales normalmente:", 
+      dimension: "TrabajoObjetivos"
+    }, 
+    {
+      id: 22, 
+      text: "Trabajo y objetivos:", 
+      dimension: "TrabajoObjetivos"
+    }, 
+    {
+      id: 23, 
+      text: "Cuando tienes libertad total:", 
+      dimension: "TrabajoObjetivos"
+    }, 
+    {
+      id: 24, 
+      text: "¿Qué suele motivarte más?", 
+      dimension: "TrabajoObjetivos"
+    }, 
+    {
+      id: 25, 
+      text: "¿Qué describe mejor tu energía laboral?", 
+      dimension: "TrabajoObjetivos"
+    }
+] 
